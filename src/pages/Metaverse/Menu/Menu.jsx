2ecf6a194d3bbd7b071cb41ec0a095e7f561b742
Fabian Valencia <fabian.cordoba@correounivalle.edu.co>
2023-08-04 21:06:55 -0500

    Implementing icon menu

---
+++ src/pages/Metaverse/Menu/Menu.jsx
@@ -0,0 +1,18 @@
+import { useUser } from "../../../context/userContext";
+import "./menu.css"
+
+const Menu = () => {
+    const { user } = useUser();
+    const modelSrcGLB = user.avatarUrl;
+    const modelSrcPNG = modelSrcGLB.replace(".glb", ".png");
+
+    return (
+        <div className="container-user">
+            <button className="button-user">
+                <img className="icon-user" src={modelSrcPNG} alt="user" />
+            </button>
+        </div>
+    )
+}
+
+export default Menu;
