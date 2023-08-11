f2db0d934632d446d13b50fed29c766b13b876a3
Fabian Valencia <fabian.cordoba@correounivalle.edu.co>
2023-08-10 20:43:56 -0500

    Profile, menu, implemented

--- src/pages/Metaverse/Menu/Menu.jsx
+++ src/pages/Metaverse/Menu/Menu.jsx
@@ -1,24 +1,89 @@
-import { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { useUser } from "../../../context/userContext";
 import "./menu.css";
 import Profile from "../Profile/Profile";
+import { Link } from "react-router-dom";
+import Instructive from "../Instructive/Instructive";
 
 const Menu = () => {
   const { user } = useUser();
   const [showProfile, setShowProfile] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [showInstructive, setShowInstructive] = useState(false);
+
+  const image = useMemo(() => {
+    return <img className="icon-avatar" src={user.avatarPng} alt="user" />;
+  }, [user.avatarPng]);
+
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
+    setShowProfile(false);
+    setShowInstructive(false);
+  };
+
+  const handleProfileClick = () => {
+    setShowProfile(!showProfile);
+    setShowMenu(false);
+    setShowInstructive(false);
+  };
+
+  const handleInstructiveClick = () => {
+    setShowInstructive(!showInstructive);
+    setShowMenu(false);
+    setShowProfile(false);
+  };
 
   return (
     <>
       <div className="container-menu">
-        <button
-          className="button-avatar"
-          role="button"
-          onClick={() => setShowProfile(!showProfile)}
-        >
-          <img className="icon-avatar" src={user.avatarPng} alt="user" />
-        </button>
+        <div className="menu">
+          {!showProfile && !showInstructive && (
+            <button
+              className="button-avatar"
+              type="button"
+              role="button"
+              onClick={handleMenuClick}
+            >
+              {image}
+            </button>
+          )}
+          {(showProfile || showInstructive) && (
+            <button
+              className="button-exit"
+              type="button"
+              role="button"
+              onClick={handleMenuClick}
+            >
+              x
+            </button>
+          )}
+          {showMenu && !showProfile && !showInstructive && (
+            <nav className="menu-nav">
+              <ul>
+                <li>
+                  <Link onClick={handleProfileClick}>Perfil</Link>
+                </li>
+                <li>
+                  <Link onClick={() => alert("Alu is coming soon!")}>Alu</Link>
+                </li>
+                <li>
+                  <Link onClick={handleInstructiveClick}>Instructivo</Link>
+                </li>
+                <li>
+                  <Link onClick={() => alert("Configuration is coming soon!")}>
+                    Configuración
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/"}>Cerrar sesión</Link>
+                </li>
+              </ul>
+            </nav>
+          )}
+        </div>
       </div>
       {showProfile && <Profile />}
+      {showInstructive && <Instructive />}
     </>
   );
 };
