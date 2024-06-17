dd83b6340876e2e8f9c74d263d065185fec506b6
Fabian Stiven Valencia Cordoba <fabian.cordoba@correounivalle.edu.co>
2024-06-17 02:06:20 -0500

    Footer implemented

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,42 @@
-import { Link } from "react-router-dom"
-import Credits from "../Credits/Credits"
-import "./stylesFooter.css"
+import React, { useMemo } from "react";
+import "./stylesFooter.css";
 
-export default function Footer(){
-    return <div className="container-footer">
-        <span> Copyright © 2023-2024 </span>
+export default function Footer() {
+  const teachers = useMemo(
+    () => [
+      {
+        name: "Ing. Fabian Stiven Valencia Cordoba",
+        email: "fabian.cordoba@correounivalle.edu.co",
+      },
+      {
+        name: "Paola Johanna Rodriguez Carrillo PhD",
+        email: "paola.rodriguez@correounivalle.edu.co",
+      },
+      {
+        name: "Javier Mauricio Reyes PhD",
+        email: "javier.reyes@correounivalle.edu.co",
+      },
+    ],
+    []
+  );
+
+  return (
+    <footer className="footer">
+      <div>
+        <h4>Creado por los Profesores de la Universidad del Valle:</h4>
         <ul>
-            <a > Créditos </a>
+          {teachers.map((teacher) => (
+            <li key={teacher.email}>
+              <a href={`mailto:${teacher.email}`}>
+                <span>{teacher.name}</span>
+              </a>
+            </li>
+          ))}
         </ul>
-    </div>
+      </div>
+      <div>
+        <h4>&copy; Universidad del Valle - 2024</h4>
+      </div>
+    </footer>
+  );
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,11 +2,12 @@
  * Component for rendering the login page of the EISC Metaverse application.
  * This component includes subcomponents for logging in with a Univalle user or as a guest.
  */
-import "./login.css";
+import "./stylesLogin.css";
 import UnivalleUserLogin from "./UnivalleUserLogin/UnivalleUserLogin";
 import GuestLogin from "./GuestLogin/GuestLogin";
 import TitleEISC from "../../components/TitleEISC/TitleEISC";
 import Footer from "../../components/Footer/Footer";
+import { Fragment } from "react";
 
 /**
  * Login functional component.
@@ -14,20 +15,25 @@ import Footer from "../../components/Footer/Footer";
  */
 const Login = () => {
   return (
-    <div className="container-login">
-      {/* Title of the EISC Metaverse */}
-      <TitleEISC />
+    // Container for the login page
+    <Fragment>
+      <div className="container-login">
+        {/* Title of the EISC Metaverse */}
+        <TitleEISC />
 
-      {/* Login card containing Univalle user login and guest login options */}
-      <div className="card-login">
-        {/* Component for logging in with a Univalle user */}
-        <UnivalleUserLogin />
+        {/* Login card containing Univalle user login and guest login options */}
+        <div className="card-login">
+          {/* Component for logging in with a Univalle user */}
+          <UnivalleUserLogin />
 
-        {/* Component for logging in as a guest */}
-        <GuestLogin />
+          {/* Component for logging in as a guest */}
+          <GuestLogin />
+
+          {/* Component for footer */}
+        </div>
       </div>
-      <Footer/>
-    </div>
+      <Footer />
+    </Fragment>
   );
 };
 
